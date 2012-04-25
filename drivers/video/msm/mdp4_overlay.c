2e8ea4407a134d204b0f4e045e199d3e0b821a3a
Ravishangar Kalyanam <rkalya@codeaurora.org>
2012-04-25 17:59:15 +0530

    msm_fb: display: Unstage video from primary mixer H/W on unset
    Resolve video playback suspend/resume IOMMU page fault by
    unstaging video layer on VG pipes from primary mixer H/W during
    overlay_unset calls immediately at the next vsync boundary
    
    CRs-Fixed: 350371
    CRs-Fixed: 354236
    Signed-off-by: Ravishangar Kalyanam <rkalya@codeaurora.org>
    Change-Id: I7fe10989a7bc3d7f379adfd18010c5e8cd655368
    
    Conflicts:
    
    	drivers/video/msm/mdp4_overlay.c

--- drivers/video/msm/mdp4_overlay.c
+++ drivers/video/msm/mdp4_overlay.c
@@ -2498,9 +2498,9 @@ int mdp4_overlay_unset(struct fb_info *info, int ndx)
 
 	if (mfd->mdp_rev >= MDP_REV_41 &&
 		mdp4_overlay_is_rgb_type(pipe->src_format) &&
-		!mfd->use_ov0_blt &&
-		(pipe->mixer_num == MDP4_MIXER0)) {
-		ctrl->stage[pipe->mixer_num][pipe->mixer_stage] = NULL;
+		!mfd->use_ov0_blt && (pipe->mixer_num == MDP4_MIXER0 ||
+		hdmi_prim_display)) {
+			ctrl->stage[pipe->mixer_num][pipe->mixer_stage] = NULL;
 	} else {
 		if (!mdp4_overlay_is_rgb_type(pipe->src_format))
 			mdp4_overlay_bg_solidfill_clear(pipe);

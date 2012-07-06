7cee36e18a189ae612e3e57c8f03235c07036b74
Mike Kasick <mike@kasick.org>
2012-07-06 01:59:15 -0400

    Add ram_console support.
    - Place in the last 1 MB of RAM, in high memory.
    - Exclude the final 4 kB for the kexec hardboot page.

--- arch/arm/mach-msm/board-m2_spr.c
+++ arch/arm/mach-msm/board-m2_spr.c
@@ -1029,6 +1029,25 @@ static int __init ext_display_setup(char *param)
 }
 early_param("ext_display", ext_display_setup);
 
+/* Exclude the last 4 kB to preserve the kexec hardboot page. */
+#ifdef CONFIG_ANDROID_RAM_CONSOLE
+#define RAM_CONSOLE_START 0xfff00000
+#define RAM_CONSOLE_SIZE  (SZ_1M-SZ_4K)
+
+static struct resource ram_console_resource[] = {
+	{
+		.flags = IORESOURCE_MEM,
+	},
+};
+
+static struct platform_device ram_console_device = {
+	.name          = "ram_console",
+	.id            = -1,
+	.num_resources = ARRAY_SIZE(ram_console_resource),
+	.resource      = ram_console_resource,
+};
+#endif
+
 unsigned int address = 0xea000000;
 unsigned int size = 0x100000;
 
@@ -1054,6 +1073,13 @@ static void __init msm8960_reserve(void)
 		ret = memblock_remove(address, size);
 		BUG_ON(ret);
 	}
+
+#ifdef CONFIG_ANDROID_RAM_CONSOLE
+	if (memblock_remove(RAM_CONSOLE_START, RAM_CONSOLE_SIZE) == 0) {
+		ram_console_resource[0].start = RAM_CONSOLE_START;
+		ram_console_resource[0].end   = RAM_CONSOLE_START+RAM_CONSOLE_SIZE-1;
+	}
+#endif
 }
 
 static int msm8960_change_memory_power(u64 start, u64 size,
@@ -4416,6 +4442,9 @@ static struct platform_device *m2_spr_devices[] __initdata = {
 #ifdef CONFIG_VIBETONZ
 	&vibetonz_device,
 #endif /* CONFIG_VIBETONZ */
+#ifdef CONFIG_ANDROID_RAM_CONSOLE
+	&ram_console_device,
+#endif
 };
 
 static void __init msm8960_i2c_init(void)

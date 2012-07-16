dc98f7190e4cddd60d6a1dd903bcb975c5d0e0e2
Ravishangar Kalyanam <rkalya@codeaurora.org>
2012-07-16 15:08:13 -0700

    msm_fb: display: Increase MDP IOMMU buffer mapping size
    Double the MDP IOMMU buffer mapping size to allow overfetching
    by hardware for boundary conditions and avoid system crashes
    due to page faults.
    
    CRs-Fixed: 378108
    CRs-Fixed: 364478
    Signed-off-by: Ravishangar Kalyanam <rkalya@codeaurora.org>
    Change-Id: Ib19af6b06ab06455acaffcca33830e0487bb0836
    (cherry picked from commit 1ed0dae869f6384d23322b78691852d70c573174)
    
    Conflicts:
    
    	drivers/video/msm/mdp4_overlay.c
    
    Signed-off-by: Dhivya Subramanian <dthiru@codeaurora.org>
    
    Conflicts:
    
    	drivers/video/msm/mdp4_overlay.c

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -1444,7 +1444,7 @@ static int msm_fb_register(struct msm_fb_data_type *mfd)
 	msm_iommu_map_contig_buffer(fbi->fix.smem_start,
 					DISPLAY_WRITE_DOMAIN,
 					GEN_POOL,
-					fbi->fix.smem_len,
+					fbi->fix.smem_len * 2,
 					SZ_4K,
 					0,
 					&(mfd->display_iova));
@@ -1452,7 +1452,7 @@ static int msm_fb_register(struct msm_fb_data_type *mfd)
 	msm_iommu_map_contig_buffer(fbi->fix.smem_start,
 					DISPLAY_READ_DOMAIN,
 					GEN_POOL,
-					fbi->fix.smem_len,
+					fbi->fix.smem_len * 2,
 					SZ_4K,
 					0,
 					&(mfd->display_iova));
@@ -1460,7 +1460,7 @@ static int msm_fb_register(struct msm_fb_data_type *mfd)
 	msm_iommu_map_contig_buffer(fbi->fix.smem_start,
 					ROTATOR_SRC_DOMAIN,
 					GEN_POOL,
-					fbi->fix.smem_len,
+					fbi->fix.smem_len * 2,
 					SZ_4K,
 					0,
 					&(mfd->rotator_iova));

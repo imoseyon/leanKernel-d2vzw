9aad45c420c43c5e43ed4a4d63e2b6b05e863140
imoseyon <imoseyon@gmail.com>
2012-07-29 20:55:07 -0700

    build script mods

--- buildlean.sh
+++ buildlean.sh
@@ -1,5 +1,4 @@
-#make ARCH=arm CROSS_COMPILE=/root/CodeSourcery/Sourcery_G++_Lite/bin/arm-none-linux-gnueabi- -j2
-#make ARCH=arm CROSS_COMPILE=/data/linaro/android-toolchain-eabi/bin/arm-linux-androideabi- -j2
+make ARCH=arm CROSS_COMPILE=/data/linaro/android-toolchain-eabi/bin/arm-linux-androideabi- -j2
 find drivers -name "*.ko" | xargs /data/linaro/android-toolchain-eabi/bin/arm-linux-androideabi-strip --strip-unneeded
 find drivers -name "*.ko" | xargs -i cp {} zip/system/lib/modules/
 cp arch/arm/boot/zImage kexec/kexec/kernel
@@ -13,6 +12,4 @@ mv kexec-boot.zip ../zip/system/etc
 cd ../zip
 zip -r lk_tw_testv${1}.zip *
 mv lk_tw_testv${1}.zip /tmp
-
-#/data/unpack-mkbootimg/mkbootimg --cmdline 'console=null androidboot.hardware=qcom user_debug=31' --base 0x80200000 --ramdiskaddr 0x81500000 --kernel arch/arm/boot/zImage --ramdisk /tmp/lk-ramdisk.cpio.gz -o /tmp/lkboot6.img
-#bspatch /tmp/lkboot.img /tmp/lkboot4.img imagediff.patch 
+/data/utils/s3_ftpupload.sh $1

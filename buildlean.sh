e52c0457dd7ab0a873e796627f7ae2490f18352d
imoseyon <imoseyon@gmail.com>
2012-07-30 22:59:24 -0700

    build script mods

--- buildlean.sh
+++ buildlean.sh
@@ -1,4 +1,6 @@
-make ARCH=arm CROSS_COMPILE=/data/linaro/android-toolchain-eabi/bin/arm-linux-androideabi- -j2
+#!/bin/bash
+[[ $2 == "compile" ]] &&
+  make ARCH=arm CROSS_COMPILE=/data/linaro/android-toolchain-eabi/bin/arm-linux-androideabi- -j2
 find drivers -name "*.ko" | xargs /data/linaro/android-toolchain-eabi/bin/arm-linux-androideabi-strip --strip-unneeded
 find drivers -name "*.ko" | xargs -i cp {} zip/system/lib/modules/
 cp arch/arm/boot/zImage kexec/kexec/kernel
@@ -12,4 +14,4 @@ mv kexec-boot.zip ../zip/system/etc
 cd ../zip
 zip -r lk_tw_testv${1}.zip *
 mv lk_tw_testv${1}.zip /tmp
-/data/utils/s3_ftpupload.sh $1
+[[ $2 == "upload" ]] && /data/utils/s3_ftpupload.sh $1

83ffe8fb572cc7d17615c97d3b34d72124280b07
imoseyon <imoseyon@gmail.com>
2012-08-05 12:34:41 -0700

    build script: enhancements

--- buildlean.sh
+++ buildlean.sh
@@ -1,8 +1,9 @@
-#make ARCH=arm CROSS_COMPILE=/root/CodeSourcery/Sourcery_G++_Lite/bin/arm-none-linux-gnueabi- -j2
-#make ARCH=arm CROSS_COMPILE=/data/linaro/android-toolchain-eabi/bin/arm-linux-androideabi- -j2
+#!/bin/bash
+[[ $2 == "compile" ]] &&
+  make ARCH=arm CROSS_COMPILE=/data/linaro/android-toolchain-eabi/bin/arm-linux-androideabi- -j2
+find drivers -name "*.ko" | xargs /data/linaro/android-toolchain-eabi/bin/arm-linux-androideabi-strip --strip-unneeded
+find drivers -name "*.ko" | xargs -i cp {} zip/system/lib/modules/
 cp arch/arm/boot/zImage kexec/kexec/kernel
-cp drivers/net/wireless/bcmdhd/dhd.ko zip/system/lib/modules
-cp drivers/net/wireless/btlock/btlock.ko zip/system/lib/modules
 cd cm.ramdisk
 chmod 750 init*
 chmod 644 default* uevent* MSM*
@@ -11,8 +12,6 @@ cd ../kexec
 zip -r kexec-boot.zip *
 mv kexec-boot.zip ../zip/system/etc
 cd ../zip
-zip -r lk_aosp_testv${1}.zip *
-mv lk_aosp_testv${1}.zip /tmp
-
-#/data/unpack-mkbootimg/mkbootimg --cmdline 'console=null androidboot.hardware=qcom user_debug=31' --base 0x80200000 --ramdiskaddr 0x81500000 --kernel arch/arm/boot/zImage --ramdisk /tmp/lk-ramdisk.cpio.gz -o /tmp/lkboot6.img
-#bspatch /tmp/lkboot.img /tmp/lkboot4.img imagediff.patch 
+zip -r lk_aosp_jb_testv${1}.zip *
+mv lk_aosp_jb_testv${1}.zip /tmp
+[[ $2 == "upload" ]] && /data/utils/s3_ftpupload.sh $1

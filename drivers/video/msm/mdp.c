f7f014bcd505cad734b499349620278aa6eabd47
Mayank Chopra <makchopra@codeaurora.org>
2012-08-06 19:43:42 +0530

    msm_fb: display: Update mdp clock counter for unsuccessful probes
    If a panel probe does not succeed, call mdp_clk_ctrl to maintain
    mdp_clk_cnt counter else mismatch in counter results in mdp clock
    to never go off in cases like suspend-resume.
    
    CRs-Fixed: 385560
    Change-Id: I4ca54b051af98e823dc7f3ff7b9bcb960532c7e8
    Signed-off-by: Mayank Chopra <makchopra@codeaurora.org>

--- drivers/video/msm/mdp.c
+++ drivers/video/msm/mdp.c
@@ -2514,6 +2514,7 @@ static int mdp_probe(struct platform_device *pdev)
 		else {
 			printk(KERN_ERR "Invalid Selection of destination panel\n");
 			rc = -ENODEV;
+			mdp_clk_ctrl(0);
 			goto mdp_probe_err;
 		}
 
@@ -2556,6 +2557,7 @@ static int mdp_probe(struct platform_device *pdev)
 		else {
 			printk(KERN_ERR "Invalid Selection of destination panel\n");
 			rc = -ENODEV;
+			mdp_clk_ctrl(0);
 			goto mdp_probe_err;
 		}
 		INIT_WORK(&mfd->dma_update_worker,
@@ -2648,6 +2650,7 @@ static int mdp_probe(struct platform_device *pdev)
 				pr_err("%s: writeback panel not supprted\n",
 					 __func__);
 				platform_device_put(msm_fb_dev);
+				mdp_clk_ctrl(0);
 				return -ENODEV;
 			}
 			pdata->on = mdp4_overlay_writeback_on;

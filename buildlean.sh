49b461b1f36ba58dd2a6f546da070191cc5c696a
imoseyon <imoseyon@gmail.com>
2012-08-09 19:58:20 -0700

    build: change to beta

--- buildlean.sh
+++ buildlean.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 [[ $2 == "compile" ]] &&
+  sed -i s/CONFIG_LOCALVERSION=\".*\"/CONFIG_LOCALVERSION=\"-leanKernel-${1}\"/ .config &&
   make ARCH=arm CROSS_COMPILE=/data/linaro/android-toolchain-eabi/bin/arm-linux-androideabi- -j2
 find drivers -name "*.ko" | xargs /data/linaro/android-toolchain-eabi/bin/arm-linux-androideabi-strip --strip-unneeded
 find drivers -name "*.ko" | xargs -i cp {} zip/system/lib/modules/
@@ -12,6 +13,6 @@ cd ../kexec
 zip -r kexec-boot.zip *
 mv kexec-boot.zip ../zip/system/etc
 cd ../zip
-zip -r lk_tw_testv${1}.zip *
-mv lk_tw_testv${1}.zip /tmp
+zip -r lk_tw_beta-v${1}.zip *
+mv lk_tw_beta-v${1}.zip /tmp
 [[ $2 == "upload" ]] && /data/utils/s3_ftpupload.sh $1

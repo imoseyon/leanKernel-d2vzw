8ac799b8bdd2c7b9f05b997485b007cc1186a5d9
imoseyon <imoseyon@gmail.com>
2012-08-13 20:21:43 -0700

    build script update

--- buildlean.sh
+++ buildlean.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 [[ $2 == "compile" ]] &&
+  sed -i s/CONFIG_LOCALVERSION=\".*\"/CONFIG_LOCALVERSION=\"-leanKernel-${1}\"/ .config &&
   make ARCH=arm CROSS_COMPILE=/data/linaro/android-toolchain-eabi/bin/arm-linux-androideabi- -j2
 find drivers -name "*.ko" | xargs /data/linaro/android-toolchain-eabi/bin/arm-linux-androideabi-strip --strip-unneeded
 find drivers -name "*.ko" | xargs -i cp {} zip/system/lib/modules/
@@ -14,4 +15,4 @@ mv kexec-boot.zip ../zip/system/etc
 cd ../zip
 zip -r lk_aosp_jb_beta-v${1}.zip *
 mv lk_aosp_jb_beta-v${1}.zip /tmp
-[[ $2 == "upload" ]] && /data/utils/s3_ftpupload.sh $1
+[[ $2 == "upload" ]] && /data/utils/s3_ftpupload2.sh $1

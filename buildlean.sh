8276517afad90ad7363b5a6ccb755c8e95766d9a
imoseyon <imoseyon@gmail.com>
2012-08-15 22:58:42 -0700

    build script changes for non-kexec

--- buildlean.sh
+++ buildlean.sh
@@ -4,15 +4,13 @@
   make ARCH=arm CROSS_COMPILE=/data/linaro/android-toolchain-eabi/bin/arm-linux-androideabi- -j2
 find drivers -name "*.ko" | xargs /data/linaro/android-toolchain-eabi/bin/arm-linux-androideabi-strip --strip-unneeded
 find drivers -name "*.ko" | xargs -i cp {} zip/system/lib/modules/
-cp arch/arm/boot/zImage kexec/kexec/kernel
 cd cm.ramdisk
 chmod 750 init*
 chmod 644 default* uevent* MSM*
-find . | cpio -o -H newc | gzip > ../kexec/kexec/ramdisk.img
-cd ../kexec
-zip -r kexec-boot.zip *
-mv kexec-boot.zip ../zip/system/etc
-cd ../zip
+find . | cpio -o -H newc | gzip > /tmp/ramdisk.img
+cd ../
+/data/unpack-mkbootimg/mkbootimg --cmdline 'console=null androidboot.hardware=qcom user_debug=31' --base 0x80200000 --ramdiskaddr 0x81500000 --kernel arch/arm/boot/zImage --ramdisk /tmp/ramdisk.img -o zip/boot.img
+cd zip
 zip -r lk_aosp_jb_beta-v${1}.zip *
 mv lk_aosp_jb_beta-v${1}.zip /tmp
 [[ $2 == "upload" ]] && /data/utils/s3_ftpupload2.sh $1

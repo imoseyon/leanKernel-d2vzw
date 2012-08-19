4c1de269f5c6ae752b6a3ac180da57d2a9b6b25a
imoseyon <imoseyon@gmail.com>
2012-08-18 20:23:27 -0700

    fastcharge v2: hopefully this fixes some lingering issues

--- arch/arm/mach-msm/board-m2_vzw.c
+++ arch/arm/mach-msm/board-m2_vzw.c
@@ -189,6 +189,8 @@ static unsigned char hdmi_is_primary = 1;
 static unsigned char hdmi_is_primary;
 #endif
 
+extern int force_fast_charge;
+
 static struct platform_device msm_fm_platform_init = {
 	.name = "iris_fm",
 	.id   = -1,
@@ -1604,7 +1606,8 @@ int msm8960_get_cable_type(void)
 #endif
 			break;
 		case CABLE_TYPE_USB:
-			fsa9485_usb_cb(1);
+			if (!force_fast_charge) fsa9485_usb_cb(1);
+			  else fsa9485_charger_cb(1);
 			break;
 		case CABLE_TYPE_AC:
 			fsa9485_charger_cb(1);

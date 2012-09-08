f2a788bbde5971a00cfe1011f5d27ea9aa32b923
imoseyon <imoseyon@gmail.com>
2012-09-07 20:53:28 -0700

    build script: set proper permissions

--- buildlean.sh
+++ buildlean.sh
@@ -7,8 +7,8 @@
 find drivers -name "*.ko" | xargs /data/linaro/android-toolchain-eabi/bin/arm-linux-androideabi-strip --strip-unneeded
 find drivers -name "*.ko" | xargs -i cp {} zip/system/lib/modules/
 cd cm.ramdisk
-chmod 750 init*
-chmod 644 default* uevent* MSM*
+chmod 750 init* charger
+chmod 644 default* uevent*
 find . | cpio -o -H newc | gzip > /tmp/ramdisk.img
 cd ../
 /data/unpack-mkbootimg/mkbootimg --cmdline 'console=null androidboot.hardware=qcom user_debug=31' --base 0x80200000 --ramdiskaddr 0x81500000 --kernel arch/arm/boot/zImage --ramdisk /tmp/ramdisk.img -o zip/boot.img

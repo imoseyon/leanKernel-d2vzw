5127d0a51ecae14e711fff5f52415e225379183b
Pavankumar Kondeti <pkondeti@codeaurora.org>
2012-09-18 17:52:51 +0530

    USB: Allow skipping device resume during system resume
    Some buses would like to keep their devices in suspend state during system
    resume.  For example HSIC controller driver prevent system suspend when
    HSIC device is active.  If this device is resumed, during system resume
    HSIC controller acquire wake lock and prevent the subsequent suspend till
    HSIC device is auto suspended.
    
    CRs-Fixed: 403975
    Change-Id: I93ccd874bf489190e4afb94b533db47bb2911db8
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- drivers/usb/core/driver.c
+++ drivers/usb/core/driver.c
@@ -1371,6 +1371,9 @@ int usb_suspend(struct device *dev, pm_message_t msg)
 {
 	struct usb_device	*udev = to_usb_device(dev);
 
+	if (udev->bus->skip_resume && udev->state == USB_STATE_SUSPENDED)
+		return 0;
+
 	unbind_no_pm_drivers_interfaces(udev);
 
 	/* From now on we are sure all drivers support suspend/resume
@@ -1395,7 +1398,16 @@ int usb_resume(struct device *dev, pm_message_t msg)
 			do_rebind_interfaces(udev);
 		status = 0;
 
-	/* For all other calls, take the device back to full power and
+	/*
+	 * Some buses would like to keep their devices in suspend
+	 * state after system resume.  Their resume happen when
+	 * a remote wakeup is detected or interface driver start
+	 * I/O.
+	 */
+	if (udev->bus->skip_resume)
+		return 0;
+
+	/* For all calls, take the device back to full power and
 	 * tell the PM core in case it was autosuspended previously.
 	 * Unbind the interfaces that will need rebinding later,
 	 * because they fail to support reset_resume.

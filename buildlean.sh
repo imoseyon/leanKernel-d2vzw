20e72636a7876f73e16a620f305bd2877c65db6d
imoseyon <imoseyon@gmail.com>
2012-09-24 21:32:53 -0700

    build: remove "beta" and clean up a bit
    Conflicts:
    
    	buildlean.sh

--- buildlean.sh
+++ buildlean.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+prefix="lk_tw_ics"
+device="vzw"
+filename="${prefix}_${device}-v${1}.zip"
+
 [[ `diff arch/arm/configs/lk_defconfig .config ` ]] && \
         { echo "Unmatched defconfig!"; exit -1; }
 [[ $2 == "compile" ]] &&
@@ -14,6 +18,8 @@ find . | cpio -o -H newc | gzip > /tmp/ramdisk.img
 cd ../
 /data/unpack-mkbootimg/mkbootimg --cmdline 'console=null androidboot.hardware=qcom user_debug=31' --base 0x80200000 --ramdiskaddr 0x81500000 --kernel arch/arm/boot/zImage --ramdisk /tmp/ramdisk.img -o zip/boot.img
 cd zip
-zip -r lk_tw_beta-v${1}.zip *
-mv lk_tw_beta-v${1}.zip /tmp
+zip -r $filename *
+mv $filename /tmp
 [[ $2 == "upload" ]] && /data/utils/s3_ftpupload.sh $1
+echo
+md5sum /tmp/$filename

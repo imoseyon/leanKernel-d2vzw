747cdb47983b73fbb8cf92bcb7ae0bc787501526
Steve Kondik <shade@chemlab.org>
2012-11-09 10:53:03 -0800

    video: msm: Reset MDP clock/bw rates during panel on
     * These need to be reset so that the correct rate is calculated on
       wakeup. Without this, underrun will occur.
     * Via Samsung's JB code drop.
    
    Change-Id: I02ab8a220db442c35eb5838ab6a986467d0f44ad

--- drivers/video/msm/mipi_dsi.c
+++ drivers/video/msm/mipi_dsi.c
@@ -143,6 +143,7 @@ static int mipi_dsi_off(struct platform_device *pdev)
 	return ret;
 }
 
+extern struct mdp4_overlay_perf perf_current;
 static int mipi_dsi_on(struct platform_device *pdev)
 {
 	int ret = 0;
@@ -348,6 +349,8 @@ static int mipi_dsi_on(struct platform_device *pdev)
 
 #ifdef CONFIG_MSM_BUS_SCALING
 	mdp_bus_scale_update_request(2);
+	perf_current.mdp_bw = OVERLAY_PERF_LEVEL4;
+	perf_current.mdp_clk_rate = 0;
 #endif
 
 	mdp4_overlay_dsi_state_set(ST_DSI_RESUME);

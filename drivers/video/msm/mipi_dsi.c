18a17007551493c71b3edfcd1e64985a2e964b3b
Limei Wang <e12499@motorola.com>
2012-11-26 17:44:53 -0600

    (CR) msm_fb: display: reduce turn-on display time.
        When turning on panel, increase its priority and restore it
        after panel has been turned on to reduce Turn-on display
        time when press power key.
    
        cherry pick commit:c43ca6a4 from main-jb-qc branch.
    
    Change-Id: Ie0e8e55fc7498a6b2d43ad9e1995fa96c609c81e
    Reviewed-on: http://gerrit.pcs.mot.com/500101
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Bang Nguyen <BNGUYEN1@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    
    Conflicts:
    
    	drivers/video/msm/mipi_dsi.c
    
    Signed-off-by: Luis Cruz <ljc2491@gmail.com>

--- drivers/video/msm/mipi_dsi.c
+++ drivers/video/msm/mipi_dsi.c
@@ -160,6 +160,7 @@ static int mipi_dsi_on(struct platform_device *pdev)
 	u32 dummy_xres, dummy_yres;
 	u32 tmp;
 	int target_type = 0;
+	int old_nice;
 
 	pr_debug("%s+:\n", __func__);
 
@@ -167,6 +168,14 @@ static int mipi_dsi_on(struct platform_device *pdev)
 	fbi = mfd->fbi;
 	var = &fbi->var;
 	pinfo = &mfd->panel_info;
+	/*
+	 * Now first priority is to turn on LCD quickly for better
+	 * user experience. We set current task to higher priority
+	 * and restore it after panel is on.
+	 */
+	old_nice = task_nice(current);
+	if (old_nice > -20)
+		set_user_nice(current, -20);
 
 	if (mipi_dsi_pdata && mipi_dsi_pdata->dsi_power_save)
 		mipi_dsi_pdata->dsi_power_save(1);
@@ -344,6 +353,10 @@ static int mipi_dsi_on(struct platform_device *pdev)
 		mutex_unlock(&mfd->dma->ov_mutex);
 	else
 		up(&mfd->dma->mutex);
+
+	/* Restore current priority */
+	if (old_nice > -20)
+		set_user_nice(current, old_nice);
 
 	pr_debug("%s-:\n", __func__);
 

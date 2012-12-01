87122fcc20ac7bef674a57c8c45ff4cd8bed6bcc
imoseyon <imoseyon@gmail.com>
2012-12-01 10:10:24 -0800

    cpufreq: override_vmin sysfs to allow kraitv2_apply_vmin to run
    Setting it to 1 restores the stock kernel behavior to apply vmin
    if krait_needs_vmin() returns true.
    
    Conflicts:
    
    	arch/arm/mach-msm/acpuclock-8960.c
    	drivers/cpufreq/cpufreq.c
    
    Conflicts:
    
    	arch/arm/mach-msm/acpuclock-8960.c
    	drivers/cpufreq/cpufreq.c
    
    Conflicts:
    
    	drivers/cpufreq/cpufreq.c

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -35,6 +35,8 @@
 
 unsigned int thermal_max = 1512000;
 
+int allow_vmin = 1;
+
 /**
  * The "cpufreq driver" - the arch- or hardware-dependent low
  * level driver of CPUFreq support, and its spinlock. This lock
@@ -693,6 +695,7 @@ static ssize_t show_bios_limit(struct cpufreq_policy *policy, char *buf)
 
 extern ssize_t vc_get_vdd(char *buf);
 extern void vc_set_vdd(const char *buf);
+extern void override_vmin_all();
 
 static ssize_t show_UV_mV_table(struct cpufreq_policy *policy, char *buf)
 {
@@ -702,6 +705,19 @@ static ssize_t store_UV_mV_table
 (struct cpufreq_policy *policy, const char *buf, size_t count)
 {  
         vc_set_vdd(buf);
+	return count;
+}
+static ssize_t show_override_vmin(struct cpufreq_policy *policy, char *buf)
+{
+	return sprintf(buf, "%d\n", allow_vmin);
+}
+static ssize_t store_override_vmin
+(struct cpufreq_policy *policy, const char *buf, size_t count)
+{  
+	int i;
+	char *vtable;
+	sscanf(buf, "%du", &allow_vmin);
+	if (allow_vmin) override_vmin_all();
 	return count;
 }
 
@@ -722,6 +738,7 @@ cpufreq_freq_attr_rw(scaling_governor);
 cpufreq_freq_attr_rw(scaling_setspeed);
 cpufreq_freq_attr_rw(UV_mV_table);
 cpufreq_freq_attr_rw(thermal_max_freq);
+cpufreq_freq_attr_rw(override_vmin);
 
 static struct attribute *default_attrs[] = {
 	&cpuinfo_min_freq.attr,
@@ -738,6 +755,7 @@ static struct attribute *default_attrs[] = {
 	&scaling_available_governors.attr,
 	&scaling_setspeed.attr,
 	&UV_mV_table.attr,
+	&override_vmin.attr,
 	NULL
 };
 

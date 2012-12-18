182de43da11984609cfcf4b4421ac2c76d6383f4
Steve Kondik <shade@chemlab.org>
2012-12-17 23:44:59 -0800

    msm: display: Remove dead code
    Change-Id: I8d61fab9b407849dca8c1f7c71a055f34011a6a9

--- drivers/video/msm/mdp4_overlay_dsi_video.c
+++ drivers/video/msm/mdp4_overlay_dsi_video.c
@@ -45,10 +45,6 @@ static int dsi_video_enabled;
 
 #define MAX_CONTROLLER	1
 
-extern struct mdp4_overlay_perf perf_request; 
-static struct wake_lock blt_mode_perf_up;
-static int blt_wake_lock_init = 0;
-
 static struct vsycn_ctrl {
 	struct device *dev;
 	int inited;
@@ -163,9 +159,6 @@ int mdp4_dsi_video_pipe_commit(int cndx, int wait)
 	struct mdp4_overlay_pipe *real_pipe;
 	unsigned long flags;
 	int cnt = 0;
-	bool blt_mode_perf_backup = false;
-	u32 mdp_bw_backup = 0;
-	u32 mdp_clk_backup = 0;
 
 	vctrl = &vsync_ctrl_db[cndx];
 
@@ -205,17 +198,6 @@ int mdp4_dsi_video_pipe_commit(int cndx, int wait)
 	spin_unlock_irqrestore(&vctrl->spin_lock, flags);
 
 	if(vctrl->blt_change) {
-		blt_mode_perf_backup = true; 
-		mdp_bw_backup = perf_request.mdp_bw;
-		mdp_clk_backup = perf_request.mdp_clk_rate;
-		perf_request.mdp_bw = OVERLAY_PERF_LEVEL4;
-		perf_request.mdp_clk_rate = mdp_max_clk;
-	}
-
-    mdp4_overlay_mdp_perf_upd(vctrl->mfd, 1);
-
-	if (vctrl->blt_change) {
-		wake_lock(&blt_mode_perf_up);
 		pipe = vctrl->base_pipe;
 		spin_lock_irqsave(&vctrl->spin_lock, flags);
 		INIT_COMPLETION(vctrl->dmap_comp);
@@ -225,11 +207,6 @@ int mdp4_dsi_video_pipe_commit(int cndx, int wait)
 		mdp4_dsi_video_wait4dmap(0);
 		if (pipe->ov_blt_addr)
 			mdp4_dsi_video_wait4ov(0);
-		if(blt_mode_perf_backup == true){
-			perf_request.mdp_bw = mdp_bw_backup;
-			perf_request.mdp_clk_rate = mdp_clk_backup;
-		}
-		wake_unlock(&blt_mode_perf_up);
 	}
 
 	pipe = vp->plist;
@@ -696,11 +673,6 @@ int mdp4_dsi_video_on(struct platform_device *pdev)
 
 	mdp_histogram_ctrl_all(TRUE);
 
-	if(!blt_wake_lock_init) {
-		wake_lock_init(&blt_mode_perf_up, WAKE_LOCK_IDLE, "blt_mode_wakelock");
-		blt_wake_lock_init = true;
-	}
-
 	mdp4_overlay_dsi_video_start();
 	return ret;
 }
@@ -1139,7 +1111,7 @@ void mdp4_dsi_video_overlay(struct msm_fb_data_type *mfd)
 	uint8 *buf;
 	unsigned int buf_offset;
 	int bpp;
-	int cnt, cndx = 0;
+	int cndx = 0;
 	struct vsycn_ctrl *vctrl;
 	struct mdp4_overlay_pipe *pipe;
 
@@ -1170,14 +1142,12 @@ void mdp4_dsi_video_overlay(struct msm_fb_data_type *mfd)
 
 	mdp4_overlay_mdp_perf_upd(mfd, 1);
 
-	cnt = mdp4_dsi_video_pipe_commit(0, 0); 
+	mdp4_dsi_video_pipe_commit(0, 0); 
 
-	if (cnt) { 
-		if (pipe->ov_blt_addr)
-			mdp4_dsi_video_wait4ov(0);
-		else
-			mdp4_dsi_video_wait4dmap(0);
-	}
+	if (pipe->ov_blt_addr)
+		mdp4_dsi_video_wait4ov(0);
+	else
+		mdp4_dsi_video_wait4dmap(0);
 
 	mdp4_overlay_mdp_perf_upd(mfd, 0);
 	mutex_unlock(&mfd->dma->ov_mutex);

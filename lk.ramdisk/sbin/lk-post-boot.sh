639a712fc93561648ba075b1f690382974594b01
imoseyon <imoseyon@gmail.com>
2012-12-30 20:20:14 -0800

    ramdisk:lk-postboot: log to a file when triggered

--- lk.ramdisk/sbin/lk-post-boot.sh
+++ lk.ramdisk/sbin/lk-post-boot.sh
@@ -4,7 +4,10 @@ check="qmiproxy gsiff"
 
 for i in $check; do
   etime=`busybox ps -o pid,time,args | grep $i | grep -v grep | awk '{ print $2 }' | awk -F: '{ print $2 }'`
-  [ $etime -gt 10 ] && pkill -f $i
+  if [ $etime -gt 20 ]; then
+    pkill -f $i
+    echo "$i restarted due to cpu time of $etime" >> /sdcard/lk.log
+  fi
 done
 
 # restart thermald to use new max freq set by cpu app

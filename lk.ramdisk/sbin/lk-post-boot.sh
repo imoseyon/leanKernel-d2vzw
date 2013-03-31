6bcc55136557d7081a56781cc64def0f0146b70c
imoseyon <imoseyon@gmail.com>
2013-03-31 15:48:57 -0700

    ramdisk: remove thermald hack - not needed anymore, hopefully
    Change-Id: I5190e230ed6141638afddd0789b9cb914dbfe77b

--- lk.ramdisk/sbin/lk-post-boot.sh
+++ lk.ramdisk/sbin/lk-post-boot.sh
@@ -1,15 +1,3 @@
 #!/system/xbin/busybox ash
 
-check=""
-
-for i in $check; do
-  etime=`busybox ps -o pid,time,args | grep $i | grep -v grep | awk '{ print $2 }' | awk -F: '{ print $2 }'`
-  if [ $etime -gt 20 ]; then
-    pkill -f $i
-    echo "$i restarted due to cpu time of $etime" >> /sdcard/lk.log
-  fi
-done
-
-# restart thermald to use new max freq set by cpu app
-sleep 45
-pkill thermald
+# do nothing

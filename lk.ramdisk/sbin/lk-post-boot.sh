bbb3f015f24080a7566814b78a4dff593cc781d0
imoseyon <imoseyon@gmail.com>
2013-04-03 08:16:29 -0700

    ramdisk: thermald hack - honor user set max freq better
    Change-Id: Ibc81a03ab7f2c6ea4a9bcdfc9c92aef1b2012eda

--- lk.ramdisk/sbin/lk-post-boot.sh
+++ lk.ramdisk/sbin/lk-post-boot.sh
@@ -1,5 +1,8 @@
-#!/system/xbin/busybox ash
+#!/system/bin/sh
 
-# do nothing
 sleep 45
+stop thermald
+sleep 1
+max=`cat /sys/devices/system/cpu/cpu0/cpufreq/thermal_max_freq`
+echo $max > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 start thermald

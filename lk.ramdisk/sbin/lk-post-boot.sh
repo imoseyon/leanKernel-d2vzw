8646f0ccc913ac371dd8f4a5097b198004743c00
imoseyon <imoseyon@gmail.com>
2013-04-13 14:31:48 -0700

    ramdisk: no need to restart thermald at boot
    Change-Id: I7ff2a0039cdb846e93f43fb1c8f63c5d219f4d48
    
    Conflicts:
    
    	lk.ramdisk/sbin/lk-post-boot.sh

--- lk.ramdisk/sbin/lk-post-boot.sh
+++ lk.ramdisk/sbin/lk-post-boot.sh
@@ -2,13 +2,6 @@
 
 check="qmiproxy gsiff"
 
-sleep 45
-stop thermald
-sleep 1
-max=`cat /sys/devices/system/cpu/cpu0/cpufreq/thermal_max_freq`
-echo $max > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-start thermald
-
 for i in $check; do
   etime=`busybox ps -o pid,time,args | grep $i | grep -v grep | awk '{ print $2 }' | awk -F: '{ print $2 }'`
   if [ $etime -gt 20 ]; then

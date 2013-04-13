c6928d4f5214866b8950e248c0c2462496ed717d
imoseyon <imoseyon@gmail.com>
2013-04-13 14:31:48 -0700

    ramdisk: no need to restart thermald at boot
    Change-Id: I7ff2a0039cdb846e93f43fb1c8f63c5d219f4d48

--- lk.ramdisk/sbin/lk-post-boot.sh
+++ lk.ramdisk/sbin/lk-post-boot.sh
@@ -1,8 +1,2 @@
 #!/system/bin/sh
-
-sleep 45
-stop thermald
-sleep 1
-max=`cat /sys/devices/system/cpu/cpu0/cpufreq/thermal_max_freq`
-echo $max > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-start thermald
+exit

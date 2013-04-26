471a7be24032099cf847cff22d068bfbf935c51f
imoseyon <imoseyon@gmail.com>
2013-04-25 17:53:12 -0700

    cpufreq: slightly more robust cpu0/1 policy matching code
    Change-Id: Ib9b0c999be2803f82e63fdac7dd4fd6e09ceb69d

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1820,7 +1820,7 @@ static int __cpufreq_set_policy(struct cpufreq_policy *data,
 	blocking_notifier_call_chain(&cpufreq_policy_notifier_list,
 			CPUFREQ_NOTIFY, policy);
 
-	if (policy->cpu == 1) {
+	if (policy->cpu >= 1) {
 		cpu0_policy = __cpufreq_cpu_get(0,0);
 		data->min = cpu0_policy->min;
 		data->max = cpu0_policy->max;
@@ -1848,7 +1848,7 @@ static int __cpufreq_set_policy(struct cpufreq_policy *data,
 				__cpufreq_governor(data, CPUFREQ_GOV_STOP);
 
 			/* start new governor */
-			if (policy->cpu == 1 && cpu0_policy) {
+			if (policy->cpu >= 1 && cpu0_policy) {
 				data->governor = cpu0_policy->governor;
 			} else {
 				data->governor = policy->governor;

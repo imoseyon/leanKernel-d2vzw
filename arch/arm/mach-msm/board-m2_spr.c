57238ad8ba9c12ade3fef1054dfd0fc00f74b33c
Steve Kondik <shade@chemlab.org>
2013-04-29 23:42:42 -0700

    d2: Disable standalone power collapse
     * This is causing Krait watchdog timeouts. Other Krait devices such as
       Mako experience the same failure.
     * Disable it (same as stock) to stop the crashes and allow me to sleep again.
    
    Change-Id: I2db4525b4271b5d762a537bc785f3344e5fa9be2

--- arch/arm/mach-msm/board-m2_spr.c
+++ arch/arm/mach-msm/board-m2_spr.c
@@ -4579,14 +4579,14 @@ static struct msm_rpmrs_level msm_rpmrs_levels[] = {
 		true,
 		415, 715, 340827, 475,
 	},
-
+#if defined(CONFIG_MSM_STANDALONE_POWER_COLLAPSE)
 	{
 		MSM_PM_SLEEP_MODE_POWER_COLLAPSE_STANDALONE,
 		MSM_RPMRS_LIMITS(ON, ACTIVE, MAX, ACTIVE),
 		true,
 		1300, 228, 1200000, 2000,
 	},
-
+#endif
 	{
 		MSM_PM_SLEEP_MODE_POWER_COLLAPSE,
 		MSM_RPMRS_LIMITS(ON, GDHS, MAX, ACTIVE),

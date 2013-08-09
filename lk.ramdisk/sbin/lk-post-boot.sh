8075154c58b9d5731ed477185aae092ab22e9a09
imoseyon <imoseyon@gmail.com>
2013-08-08 17:17:40 -0700

    Revert "ramdisk: quick hack to accommodate superuser on android 4.3"
    This reverts commit 71d40f189b4ead30f39655cbdb5a16260abaee5b.

--- lk.ramdisk/sbin/lk-post-boot.sh
+++ lk.ramdisk/sbin/lk-post-boot.sh
@@ -1,7 +1,2 @@
-#!/system/xbin/busybox ash
-
-# enable su support for 4.3
-if [[ `grep -c release=4.3 /system/build.prop` -ge 1 ]]; then
-  [ -f "/system/xbin/su" ] && /system/xbin/su --daemon &
-  [ -f "/system/xbin/daemonsu" ] && /system/xbin/daemonsu --auto-daemon &
-fi
+#!/system/bin/sh
+exit

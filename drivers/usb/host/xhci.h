8fb2fbc66cea4684f93eaa360fea36fddd7754d6
imoseyon <imoseyon@gmail.com>
2013-10-01 23:09:26 -0700

    Linux 3.4.63

--- drivers/usb/host/xhci.h
+++ drivers/usb/host/xhci.h
@@ -1519,6 +1519,11 @@ struct xhci_hcd {
  * (16.66 ns x 5 = 84ns) ~100ns after writing to the PORTSC register.
  */
 #define XHCI_PORTSC_DELAY	(1 << 10)
+#define XHCI_TRUST_TX_LENGTH	(1 << 10)
+#define XHCI_SPURIOUS_REBOOT	(1 << 13)
+#define XHCI_COMP_MODE_QUIRK	(1 << 14)
+#define XHCI_AVOID_BEI		(1 << 15)
+#define XHCI_PLAT		(1 << 16)
 	unsigned int		num_active_eps;
 	unsigned int		limit_active_eps;
 	/* There are two roothubs to keep track of bus suspend info for */

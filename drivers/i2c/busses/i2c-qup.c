ac23eb77577e78bc1c8e55a93f3ddc582906ab56
Dave Daynard <nardholio@gmail.com>
2013-10-05 05:17:00 -0400

    i2c-qup: de-derp
    Touchscreen optimizations cherry-pick breaks the camera on Apex (and
    probably other devices with the same camera). Restore most of the
    original i2c-qup file, while still allowing i2c to overclock for d2
    
    Change-Id: I7d6c1f8c9a1064e95d59f60d81be5c9c59512e74

--- drivers/i2c/busses/i2c-qup.c
+++ drivers/i2c/busses/i2c-qup.c
@@ -216,7 +216,7 @@ qup_i2c_interrupt(int irq, void *devid)
 		return IRQ_HANDLED;
 	}
 
-	if (unlikely(status & I2C_STATUS_ERROR_MASK)) {
+	if (status & I2C_STATUS_ERROR_MASK) {
 		dev_err(dev->dev, "QUP: I2C status flags :0x%x, irq:%d\n",
 			status, irq);
 		err = status;
@@ -229,7 +229,7 @@ qup_i2c_interrupt(int irq, void *devid)
 		goto intr_done;
 	}
 
-	if (unlikely(status1 & 0x7F)) {
+	if (status1 & 0x7F) {
 		dev_err(dev->dev, "QUP: QUP status flags :0x%x\n", status1);
 		err = -status1;
 		/* Clear Error interrupt if it's a level triggered interrupt*/
@@ -762,14 +762,14 @@ static void qup_i2c_recover_bus_busy(struct qup_i2c_dev *dev)
 recovery_end:
 	enable_irq(dev->err_irq);
 }
-
+#if defined(CONFIG_MACH_M2)
 void qup_i2c_bwreset(struct i2c_adapter *adap) {
 	struct qup_i2c_dev *dev = i2c_get_adapdata(adap);
 	mutex_lock(&dev->mlock);
 	dev->clk_ctl = 0;
 	mutex_unlock(&dev->mlock);
 }
-
+#endif
 static int
 qup_i2c_xfer(struct i2c_adapter *adap, struct i2c_msg msgs[], int num)
 {
@@ -778,17 +778,18 @@ qup_i2c_xfer(struct i2c_adapter *adap, struct i2c_msg msgs[], int num)
 	int ret;
 	int rem = num;
 	long timeout;
+	int err;
 
 	pm_runtime_get_sync(dev->dev);
 	mutex_lock(&dev->mlock);
 
-	if (unlikely(dev->suspended)) {
+	if (dev->suspended) {
 		mutex_unlock(&dev->mlock);
 		return -EIO;
 	}
 
 	/* Initialize QUP registers during first transfer */
-	if (unlikely(dev->clk_ctl == 0)) {
+	if (dev->clk_ctl == 0) {
 		int fs_div;
 		int hs_div;
 		uint32_t fifo_reg;
@@ -886,8 +887,9 @@ qup_i2c_xfer(struct i2c_adapter *adap, struct i2c_msg msgs[], int num)
 				filled = true;
 		}
 
-		ret = qup_update_state(dev, QUP_RUN_STATE);
-		if (ret < 0) {
+		err = qup_update_state(dev, QUP_RUN_STATE);
+		if (err < 0) {
+			ret = err;
 			goto out_err;
 		}
 
@@ -904,8 +906,9 @@ qup_i2c_xfer(struct i2c_adapter *adap, struct i2c_msg msgs[], int num)
 			uint32_t carry_over = 0;
 
 			/* Transition to PAUSE state only possible from RUN */
-			ret = qup_update_state(dev, QUP_PAUSE_STATE);
-			if (ret < 0) {
+			err = qup_update_state(dev, QUP_PAUSE_STATE);
+			if (err < 0) {
+				ret = err;
 				goto out_err;
 			}
 
@@ -946,8 +949,9 @@ qup_i2c_xfer(struct i2c_adapter *adap, struct i2c_msg msgs[], int num)
 						filled = true;
 				}
 			}
-			ret = qup_update_state(dev, QUP_RUN_STATE);
-			if (ret < 0) {
+			err = qup_update_state(dev, QUP_RUN_STATE);
+			if (err < 0) {
+				ret = err;
 				goto out_err;
 			}
 			dev_dbg(dev->dev, "idx:%d, rem:%d, num:%d, mode:%d\n",
@@ -956,7 +960,7 @@ qup_i2c_xfer(struct i2c_adapter *adap, struct i2c_msg msgs[], int num)
 			qup_print_status(dev);
 			timeout = wait_for_completion_timeout(&complete,
 					msecs_to_jiffies(dev->out_fifo_sz));
-			if (unlikely(!timeout)) {
+			if (!timeout) {
 				uint32_t istatus = readl_relaxed(dev->base +
 							QUP_I2C_STATUS);
 				uint32_t qstatus = readl_relaxed(dev->base +
@@ -994,7 +998,7 @@ qup_i2c_xfer(struct i2c_adapter *adap, struct i2c_msg msgs[], int num)
 				goto out_err;
 			}
 timeout_err:
-			if (unlikely(dev->err)) {
+			if (dev->err) {
 				if (dev->err > 0 &&
 					dev->err & QUP_I2C_NACK_FLAG) {
 					dev_err(dev->dev,
@@ -1045,19 +1049,23 @@ timeout_err:
 			dev_dbg(dev->dev, "pos:%d, len:%d, cnt:%d\n",
 					dev->pos, msgs->len, dev->cnt);
 		} while (dev->cnt > 0);
-		if (msgs->len == dev->pos) {
-			rem--;
-			msgs++;
-			dev->pos = 0;
-		}
-		if (rem) {
-			ret = qup_i2c_poll_clock_ready(dev);
-			if (ret < 0) {
-				goto out_err;
+		if (dev->cnt == 0) {
+			if (msgs->len == dev->pos) {
+				rem--;
+				msgs++;
+				dev->pos = 0;
 			}
-			ret = qup_update_state(dev, QUP_RESET_STATE);
-			if (ret < 0) {
-				goto out_err;
+			if (rem) {
+				err = qup_i2c_poll_clock_ready(dev);
+				if (err < 0) {
+					ret = err;
+					goto out_err;
+				}
+				err = qup_update_state(dev, QUP_RESET_STATE);
+				if (err < 0) {
+					ret = err;
+					goto out_err;
+				}
 			}
 		}
 		/* Wait for I2C bus to be idle */
@@ -1078,11 +1086,9 @@ timeout_err:
 	}
 	dev->complete = NULL;
 	dev->msg = NULL;
-	/* No need.
 	dev->pos = 0;
 	dev->err = 0;
 	dev->cnt = 0;
-	*/
 	mutex_unlock(&dev->mlock);
 	pm_runtime_mark_last_busy(dev->dev);
 	pm_runtime_put_autosuspend(dev->dev);

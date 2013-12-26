4e39bb78b58a110b51a7dcfdec832e22a3507c4f
Pavankumar Kondeti <pkondeti@codeaurora.org>
2013-12-26 13:24:40 +0530

    usb: core: Limit HCD old enumeration scheme request to root ports
    HCD_OLD_ENUM HCD flag is introduced to use old (or short) enumeration
    scheme for the devices attached on the corresponding USB bus. The old
    enumeration scheme is used by the MSM HSIC controller to avoid multiple
    reset during enumeration. This is useful when a known device is attached
    to the USB bus, which is the case with MSM HSIC. When a HSIC HUB is
    connected, any device can be connected on the HSIC USB bus. The new
    enumeration scheme supports wide variety of USB devices. Hence limit
    the old enumeration scheme request of a HCD to only root ports.
    
    Change-Id: I6035c2a766787819eb8d0e1fa774e0c79d6e958b
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -3289,7 +3289,8 @@ hub_port_init (struct usb_hub *hub, struct usb_device *udev, int port1,
 	for (i = 0; i < GET_DESCRIPTOR_TRIES; (++i, msleep(100))) {
 		if (USE_NEW_SCHEME(retry_counter) &&
 			!(hcd->driver->flags & HCD_USB3) &&
-			!(hcd->driver->flags & HCD_OLD_ENUM)) {
+			!((hcd->driver->flags & HCD_RT_OLD_ENUM) &&
+				!hdev->parent)) {
 			struct usb_device_descriptor *buf;
 			ushort idvendor;
 			int r = 0;
@@ -3393,7 +3394,8 @@ hub_port_init (struct usb_hub *hub, struct usb_device *udev, int port1,
 			msleep(10);
 			if (USE_NEW_SCHEME(retry_counter) &&
 				!(hcd->driver->flags & HCD_USB3) &&
-				!(hcd->driver->flags & HCD_OLD_ENUM))
+				!((hcd->driver->flags & HCD_RT_OLD_ENUM) &&
+					!hdev->parent))
 				break;
   		}
 

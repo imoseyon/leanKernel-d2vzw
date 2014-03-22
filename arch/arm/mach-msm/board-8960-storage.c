3eb497627437ee82a6e8273eecb806d7a644f058
infernix <infernix@gmail.com>
2014-03-22 14:20:06 -0400

    Reset parameters for EMMC to stock 3.0
    Change-Id: I0c58573c07f58ab50dd94d6c711ef023e374b68a

--- arch/arm/mach-msm/board-8960-storage.c
+++ arch/arm/mach-msm/board-8960-storage.c
@@ -385,7 +385,7 @@ static struct mmc_platform_data msm8960_sdc1_data = {
 	.pin_data	= &mmc_slot_pin_data[SDCC1],
 	.mpm_sdiowakeup_int = MSM_MPM_PIN_SDC1_DAT1,
 	.msm_bus_voting_data = &sps_to_ddr_bus_voting_data,
-	.uhs_caps2	= MMC_CAP2_HS200_1_8V_SDR,
+	.uhs_caps       = (MMC_CAP_1_8V_DDR | MMC_CAP_UHS_DDR50),
 	.packed_write	= MMC_CAP2_PACKED_WR | MMC_CAP2_PACKED_WR_CONTROL,
 };
 #endif

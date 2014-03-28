f1a281719c4ea10489da83480df46a4b497493ae
Dave Daynard <nardholio@gmail.com>
2014-03-27 23:37:44 -0400

    apexq: tweak torch high brightness
    This change does 2 things. First, it restores the previous behavior for
    Express that any value above 0 will turn the torch on. Second, it turns
    on torch low brightness before turning on high brightness for Apex,
    similar to how the flash works when taking a picture. Otherwise there is
    a risk of stuck gpio if the phone previously was rebooted with the torch
    on.
    
    Change-Id: I9f5c8800dbdd02ca2c21349a6fad9e3f3a7b0042

--- arch/arm/mach-msm/board-apexq-camera.c
+++ arch/arm/mach-msm/board-apexq-camera.c
@@ -840,8 +840,8 @@ static ssize_t cameraflash_file_cmd_store(struct device *dev,
 		gpio_set_value_cansleep(gpio_flash_set, 0);
 #endif
 		torchonoff = 0;
-	} 
-	else if (value == 1) {
+	}
+	else {
 		pr_err("[Torch flash]ON\n");
 		gpio_set_value_cansleep(gpio_flash_en, 0);
 #ifdef CONFIG_MACH_EXPRESS
@@ -864,11 +864,10 @@ static ssize_t cameraflash_file_cmd_store(struct device *dev,
 		torchonoff = 1;
 	}
 #if defined(CONFIG_MACH_APEXQ)
-	else {
+	if (value > 1) {
 		pr_err("[Torch flash]HIGH\n");
 		gpio_set_value_cansleep(gpio_flash_en, 1);
 		gpio_set_value_cansleep(gpio_flash_set, 0);
-		torchonoff = 1;
 	}
 #endif
 	return size;

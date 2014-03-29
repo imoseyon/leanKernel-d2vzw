a69085b5a10348ce12bc23356d58df96e139a9fc
Tom Tartaro <tomtartaro@gmail.com>
2014-03-28 21:56:15 -0700

    Revert "input: touchscreen: d2_fw: Update firmware"
    Change-Id: Ic1c336a1044adc0cc155b37e0c7397c1c4f0d1c7
    
    This reverts commit c2e189edf64901d15359c281889e0500448514d1.
    
    For some reason this causes either:
    -No boot past splash screen
    -Boot to lockscreen but unresponsive to touch
    
    This is only the case for some users, most users have no issue using this updated firmware. Revert until we figure out what the issue is.
    
    Change-Id: I65c8acacbfc847b71730bf18bea8eecc8f57c44c

--- drivers/input/touchscreen/mms_ts_fw.h
+++ drivers/input/touchscreen/mms_ts_fw.h
@@ -36,7 +36,7 @@ const  u8 MELFAS_binary[] = {
 	|| defined(CONFIG_MACH_SUPERIORLTE_SKT)\
 	|| defined(CONFIG_MACH_K2_KDI)
 /* 4.8" OCTA LCD */
-#define FW_VERSION 0xBD
+#define FW_VERSION 0xBB
 #include "d2_fw.h"
 /* 4.65" OCTA LCD */
 #define FW_465_VERSION 0xA8

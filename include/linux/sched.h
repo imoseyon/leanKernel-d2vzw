23590d6609538c1282abcc0cbfaad69e0235fa9c
imoseyon <imoseyon@gmail.com>
2015-01-02 10:18:35 -0800

    merge fix from linux 3.4.105

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1363,8 +1363,6 @@ struct task_struct {
 #endif
 	unsigned long atomic_flags; /* Flags needing atomic access. */
 
-	unsigned long atomic_flags; /* Flags needing atomic access. */
-
 	pid_t pid;
 	pid_t tgid;
 
